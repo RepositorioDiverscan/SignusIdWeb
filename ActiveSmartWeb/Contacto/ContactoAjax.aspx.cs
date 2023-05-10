3b942b3fb6b632aa791b043dc1a67b498cca36f9
Cristopher Salazar Arguedas <csalazar@diverscan.onmicrosoft.com>
2023-05-10 15:09:16 -0600

    Plantilla correo cliente

--- ActiveSmartWeb/Contacto/ContactoAjax.aspx.cs
+++ ActiveSmartWeb/Contacto/ContactoAjax.aspx.cs
@@ -30,7 +30,7 @@ namespace ActiveSmartWeb.Contacto
                         var Telefono = Request.Form["Telefono"];
                         var Mensaje = Request.Form["Mensaje"];
                         var Asunto = Request.Form["Asunto"];
-                        //var realizadoClientre = Correo.EnviarCorreoContactoCliente("csalazar.diverscan@gmail.com");
+                        var realizadoClientre = Correo.EnviarCorreoContactoCliente("csalazar.diverscan@gmail.com");
                         var realizado = Correo.EnviarCorreoContactoInformacion("csalazar.diverscan@gmail.com", Asunto, Nombre, Apellido, Email,Telefono,Mensaje);
 
                         Response.Write(realizado);
