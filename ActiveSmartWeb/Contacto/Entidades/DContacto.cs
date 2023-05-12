a6354d288f09d4416355bfe56b364eb8696b03df
Cristopher Salazar Arguedas <csalazar@diverscan.onmicrosoft.com>
2023-05-12 13:40:44 -0600

    Metodo InsertarContacto

---
+++ ActiveSmartWeb/Contacto/Entidades/DContacto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ActiveSmartWeb.Contacto.Entidades
+{
+    public class DContacto
+    {
+
+        public string InsertarContacto(EContacto contacto)
+        {
+            var db = DatabaseFactory.CreateDatabase("activeidsmartConnectionString");
+            var dbCommand = db.GetStoredProcCommand("SP_Registrar_Contacto");
+            db.AddInParameter(dbCommand, "@asunto", DbType.String, contacto.Asunto);
+            db.AddInParameter(dbCommand, "@nombre", DbType.String, contacto.Nombre);
+            db.AddInParameter(dbCommand, "@apellido", DbType.String, contacto.Apellido);
+            db.AddInParameter(dbCommand, "@telefono", DbType.String, contacto.Telefono);
+            db.AddInParameter(dbCommand, "@correo", DbType.String, contacto.Correo);
+            db.AddInParameter(dbCommand, "@mensaje", DbType.String, contacto.Mensaje);
+            db.AddOutParameter(dbCommand, "@Respuesta", DbType.String, 200);
+            dbCommand.CommandTimeout = 3600;
+            db.ExecuteNonQuery(dbCommand);
+            var Respuesta = db.GetParameterValue(dbCommand, "@Respuesta").ToString();
+            return Respuesta;
+        }
+
+    }
+}
---
+++ ActiveSmartWeb/Contacto/Entidades/EContacto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActiveSmartWeb.Contacto.Entidades
+{
+    public class EContacto
+    {
+        private string asunto;
+        private string nombre;
+        private string apellido;
+        private string telefono;
+        private string correo;
+        private string mensaje;
+
+        public EContacto(string asunto, string nombre, string apellido, string telefono, string correo, string mensaje)
+        {
+            this.asunto = asunto;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.telefono = telefono;
+            this.correo = correo;
+            this.mensaje = mensaje;
+        }
+
+        public EContacto()
+        {
+        }
+
+        public string Asunto { get => asunto; set => asunto = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Mensaje { get => mensaje; set => mensaje = value; }
+
+    }
+}
