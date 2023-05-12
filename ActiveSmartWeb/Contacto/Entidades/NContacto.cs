d380008a3d1b6f750eb01ba0623b1931413e1dfe
Cristopher Salazar Arguedas <csalazar@diverscan.onmicrosoft.com>
2023-05-12 13:48:07 -0600

    NContacto

---
+++ ActiveSmartWeb/Contacto/Entidades/NContacto.cs
@@ -0,0 +1,29 @@
+﻿using ActiveSmartWeb.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActiveSmartWeb.Contacto.Entidades
+{
+    public class NContacto
+    {
+
+        DContacto _dContacto = new DContacto();
+
+        public string InsertarContacto(EContacto contacto)
+        {
+            try
+            {
+                var respuesta = _dContacto.InsertarContacto(contacto);
+                return respuesta;
+            }
+            catch (Exception ex)
+            {
+                CLErrores.EscribirError(ex.Message, ex.StackTrace);
+                return "";
+            }
+        }
+
+    }
+}
